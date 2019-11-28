60ca0b27579a9e5b6fb1c3d5878060b678aeaedb
worldgu <15893851179@163.com>
2019-11-28 23:29:48 +0800

    1.通过"--server.port=8081"提供多个端口号消费提供者 2.将服务提供者同时注册到两个互相注册时服务注册中心

--- hello/src/main/java/com/zero/controller/HelloController.java
+++ hello/src/main/java/com/zero/controller/HelloController.java
@@ -1,13 +1,46 @@
 package com.zero.controller;
 
+import com.sun.istack.internal.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/hello")
+    private final Logger logger = Logger.getLogger(getClass());
+
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value="/hello",method= RequestMethod.GET)
     public String index() {
+        Map<String, List<ServiceInstance>> msl = new HashMap<>();
+        List<String> services = client.getServices();
+        for(String service : services){
+            List<ServiceInstance> sis = client.getInstances(service);
+            msl.put(service,sis);
+        }
+
         return "Hello World";
+    }
+
+    @RequestMapping(value="/serviceurl",method= RequestMethod.GET)
+    public Map<String,List<ServiceInstance>> discovery() {
+        Map<String, List<ServiceInstance>> msl = new HashMap<>();
+        List<String> services = client.getServices();
+        for(String service : services){
+            List<ServiceInstance> sis = client.getInstances(service);
+            msl.put(service,sis);
+        }
+        return msl;
     }
 }
